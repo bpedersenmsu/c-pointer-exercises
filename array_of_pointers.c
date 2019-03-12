8075a94dd5f322b3bc6d5410163096cf59fb0ca7
brianpedersen25 <b.pedersen@aspenware.com>
2019-03-12 00:36:08 -0600

    Added code for linked list

--- array_of_pointers.c
+++ array_of_pointers.c
@@ -2,10 +2,25 @@
 // Created by Ivo Georgiev on 2019-03-07.
 //
 #include <stdio.h>
+#include <stdlib.h>
 
 void run_array_of_pointers() {
     printf("Running array of pointers\n");
 
-    // YOUR CODE HERE
+    struct Data {
+        int id;
+        char testVal;
+        char name[14];
+    };
+
+    struct Data **test = (struct Data **)malloc(10 * sizeof(struct Data));
+
+    for(int i = 0; i < 10; i++){
+
+        struct Data testData = { i + 10, 'a', "PointerArrayTest"};
+        test[i] = &testData;
+        printf("Struct Id = %i and testVal = %c and Name = %s\n", test[i]->id, test[i]->testVal, test[i]->name);
+    }
+    free(test);
 
 }
--- linked_list.c
+++ linked_list.c
@@ -4,9 +4,27 @@
 
 #include <stdio.h>
 
+static struct Node {
+    int id;
+    struct Node* nextNode;
+};
+
+struct Node headNode = {1, NULL};
+struct Node tailNode = {1, NULL};
+
 void run_linked_list() {
     printf("Running linked list\n");
+    if(headNode.nextNode == NULL)
+        headNode.nextNode = &tailNode;
 
-    // YOUR CODE HERE
+    struct Node newNode = {1};
+}
 
+void insertNode(struct Node* node){
+    struct Node* current = &headNode;
+    while (current->nextNode != NULL){
+        current == current->nextNode;
+    }
+    current->nextNode = node;
+    current->nextNode->nextNode = NULL;
 }
